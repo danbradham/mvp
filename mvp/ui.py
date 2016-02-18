66979bdf0c6c50cf64080f7bcb301b3cb8b4adb8
Dan Bradham <danielbradham@gmail.com>
2016-02-18 17:52:00 -0500

    UI can now output png sequences with alpha

--- mvp/ui.py
+++ mvp/ui.py
@@ -57,25 +57,10 @@ def update_presets(dialog):
 def show():
 
     dialog = PlayblastForm.as_dialog(parent=get_maya_window())
-    dialog.camera.set_options(cmds.ls(cameras=True))
-
-    def on_accept():
-        data = dialog.get_value()
-
-        if data['preset'] == 'Current Settings':
-            state = Viewport.active().get_state()
-        else:
-            state = get_preset(data['preset'])
-
-        playblast(
-            data['filename'],
-            data['camera'],
-            state=state,
-            width=data['resolution'][0],
-            height=data['resolution'][1],
-        )
-
-    dialog.accepted.connect(on_accept)
+    cameras = cmds.ls(cameras=True)
+    for c in ['frontShape', 'sideShape', 'topShape']:
+        cameras.remove(c)
+    dialog.camera.set_options(cameras)
 
     new_button = QtGui.QPushButton('New')
     new_button.clicked.connect(partial(new_dialog, dialog))
@@ -91,26 +76,74 @@ def show():
     dialog.update_presets = update_presets
     dialog.update_presets()
 
+    ext_options = ['qt.h264', 'png', 'Custom']
+    extensions = ['.mov', '.png']
+    ext_option = controls.IntOptionControl('Ext Option', labeled=False)
+    ext_option.set_options(ext_options)
+    dialog.filename.grid.addWidget(ext_option.widget, 1, 2)
+
     path_options = ['Custom']
     path_options.extend(PATHGEN_REGISTRY.keys())
     path_option = controls.StringOptionControl('Path Option', labeled=False)
     path_option.set_options(path_options)
-    dialog.filename.grid.addWidget(path_option.widget, 1, 2)
+    dialog.filename.grid.addWidget(path_option.widget, 1, 3)
 
-    def on_path_option_change():
+    def get_path():
         path_fn = PATHGEN_REGISTRY.get(path_option.get_value(), None)
 
         if not path_fn:
             return
 
-        dialog.filename.set_value(path_fn())
+        ext = extensions[ext_option.get_value()]
+        if ext == '.png':
+            scene = os.path.splitext(cmds.file(q=True, shn=True, sn=True))[0]
+            path = os.path.join(path_fn(), scene)
+        else:
+            path = path_fn()
 
-    path_option.changed.connect(on_path_option_change)
+        path += ext
+        dialog.filename.set_value(path)
+
+    ext_option.changed.connect(get_path)
+    path_option.changed.connect(get_path)
 
     def on_filename_change():
         path_option.set_value('Custom')
+        name = dialog.filename.get_value()
+        ext = os.path.splitext(name)[-1]
+        if name.endswith('.mov'):
+            ext_option.set_value(0)
+        elif name.endswith('.png'):
+            ext_option.set_value(1)
+        else:
+            ext_option.set_value(2)
 
     dialog.filename.changed.connect(on_filename_change)
+
+    def on_accept():
+        data = dialog.get_value()
+
+        if data['preset'] == 'Current Settings':
+            state = Viewport.active().get_state()
+        else:
+            state = get_preset(data['preset'])
+
+        path, ext = os.path.splitext(data['filename'])
+        if not ext or not ext in ['.png', '.mov']:
+            ext = '.mov'
+            path += ext
+
+        playblast(
+            path,
+            data['camera'],
+            state=state,
+            width=data['resolution'][0],
+            height=data['resolution'][1],
+            format='qt' if ext == '.mov' else 'image',
+            compression='H.264' if ext == '.mov' else 'png',
+        )
+
+    dialog.accepted.connect(on_accept)
 
     dialog.setStyleSheet(stylesheet)
     dialog.show()
