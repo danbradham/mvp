6e0600fb77c1848ee52e1e43603c46ca45077717
Dan Bradham <danielbradham@gmail.com>
2019-10-28 13:31:24 -0400

    Add extension hook - Have hooks store namedtuples rather than tuples

--- mvp/hooks.py
+++ mvp/hooks.py
@@ -2,41 +2,44 @@
 
 import os
 import glob
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 from . import config
 
 
-POSTRENDER_REGISTRY = OrderedDict()
+postrender = OrderedDict()
+pathgen = OrderedDict()
+integration = OrderedDict()
+extension = OrderedDict()
+
+PathGenerator = namedtuple('PathGenerator', ['name', 'handler'])
+PostRender = namedtuple('PostRender', ['name', 'handler', 'default'])
+Extension = namedtuple('Extension', ['name', 'ext', 'handler', 'options'])
 
 
-def register_postrender(name, fn, default=None):
+def register_postrender(name, handler, default=None):
     '''Add postrender function to registry
 
     :param name: Name of postrender function (used as label in ui)
-    :param fn: Postrender function
+    :param handler: Postrender function
     '''
 
-    fn.default = default
-    POSTRENDER_REGISTRY[name] = fn
+    postrender[name] = PostRender(name, handler, default)
 
 
 def unregister_postrender(name):
     '''Remove postrender function from registry'''
 
-    POSTRENDER_REGISTRY.pop(name, None)
+    postrender.pop(name, None)
 
 
-PATHGEN_REGISTRY = OrderedDict()
-
-
-def register_path(name, fn):
+def register_path(name, handler):
     '''Add a path generator function to registry
 
     :param name: Name of path generator (used as label in ui)
-    :param fn: Path generator function
+    :param handler: Path generator function
     '''
 
-    PATHGEN_REGISTRY[name] = fn
+    pathgen[name] = PathGenerator(name, handler)
 
 
 def unregister_path(name):
@@ -45,34 +48,52 @@ def unregister_path(name):
     :param name: Name of path generator function
     '''
 
-    PATHGEN_REGISTRY.pop(name, None)
+    pathgen.pop(name, None)
 
 
-INTEGRATION_REGISTRY = OrderedDict()
-
-
-def register_integration(name, fn):
+def register_integration(name, obj):
     '''Add a path generator function to registry
 
-    :param name: Name of path generator (used as label in ui)
-    :param fn: Path generator function
+    :param name: Name of Integration
+    :param obj: Integration obj
     '''
 
-    INTEGRATION_REGISTRY[name] = fn
+    integration[name] = obj
 
 
 def unregister_integration(name):
     '''Remove path generator function from registry
 
-    :param name: Name of path generator function
+    :param name: Name of integration to remove
     '''
 
-    INTEGRATION_REGISTRY.pop(name, None)
+    integration.pop(name, None)
+
+
+def register_extension(name, ext, handler, options=None):
+    '''Register a function to handle playblasting a specific file extension
+
+    :param name: Nice name of the extension
+    :param ext: File extension
+    :param handler: Handler function that performs the playblasting
+    '''
+
+    extension[name] = Extension(name, ext, handler, options)
+
+
+def unregister_extension(name):
+    '''Register a function to handle playblasting a specific file extension
+
+    :param name: Nice name of the extension to unregister
+    '''
+
+    extension.pop(name, None)
 
 
 def init():
-    '''Import all python files in config.PRESETS_PATH'''
+    '''Discover presets'''
 
+    # Find registered presets in MVP_PRESETS path
     for path in config.PRESETS_PATH:
 
         # Import all python files on MVP_PRESETS path
@@ -85,3 +106,6 @@ def init():
         for f in glob.glob(os.path.join(path, '*', '__init__.py')):
             name = os.path.basename(os.path.dirname(f))
             __import__(name)
+
+    # Import builtin extensions
+    from . import extensions
